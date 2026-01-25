daaf3e31b25d4ef3dfbfbfb1c56bd87b424faf39
Morad <momurad.business@gmail.com>
2026-01-25 11:17:07 +0700

    first problem solved in Dict Problems

---
+++ Fundamentals/Coding/Problem-Solving-level-06-in-C#/20 Dictionary & Hashset Problems/01 - Store and Retrieve Student Grades/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Store and Retrieve Student Grades
+Problem: Store the grades of students using their names as keys, and retrieve Bob's Info using student name.
+
+Output: Student: Bob, Grade: 90
+*/
+
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+
+      Dictionary<string, int> Students = new Dictionary<string, int>();
+      Students.Add("Alice", 85);
+      Students.Add("Bob", 90);
+
+      int bobGrade = Students["Bob"];
+      Console.WriteLine("Student: Bob, Grade: " + bobGrade);
+    }
+}
